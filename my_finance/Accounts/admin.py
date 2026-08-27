d09a37032f5754a1406f073e98921acb2d706b15
Maryam Soleimani <maryamsoleimanii2003@gmail.com>
2026-08-28 00:00:49 +0330

    managing accounts

--- my_finance/Accounts/admin.py
+++ my_finance/Accounts/admin.py
@@ -2,6 +2,4 @@ from django.contrib import admin
 
 from Accounts.models import Account
 
-# Register your models here.
-
 admin.site.register(Account)
--- my_finance/Accounts/models.py
+++ my_finance/Accounts/models.py
@@ -1,4 +1,3 @@
-# backend/Accounts/models.py
 from django.db import models
 from django.contrib.auth.models import User
 
