6cd1443936493010d44927ea5fd3ae79384ece02
Maryam Soleimani <maryamsoleimanii2003@gmail.com>
2026-09-05 00:37:51 +0330

    add authentication path

--- my_finance/my_finance/urls.py
+++ my_finance/my_finance/urls.py
@@ -9,6 +9,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/', include('authentication.urls')),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/transaction/', include('Transactions.urls')),
     path('api/accounts/', include('Accounts.urls')),
