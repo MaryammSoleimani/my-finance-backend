bb0a472cb547347899a6375450d9d10d70b8af9b
Maryam Soleimani <maryamsoleimanii2003@gmail.com>
2026-09-07 14:57:54 +0330

    add a method for get balance in Toman(vahede poole Iran)

--- my_finance/Accounts/serializers.py
+++ my_finance/Accounts/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import Account
+import jdatetime
 
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
         fields = ['id', 'name', 'balance', 'is_debt','type', 'color']
         read_only_fields = ['id']
+
+        def get_balance_toman(self, obj):
+            return float(obj.balance)
